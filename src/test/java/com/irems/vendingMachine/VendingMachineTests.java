d8d156e66618a9b2faceda2c040fe33e4f6f7956
Daniel Tokai <tokaidaniel@gmail.com>
2021-10-20 16:39:59 +0200

    refactored vending machine tests

--- src/test/java/com/irems/vendingMachine/VendingMachineTests.java
+++ src/test/java/com/irems/vendingMachine/VendingMachineTests.java
@@ -13,6 +13,12 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 class VendingMachineTests {
 
 	private VendingMachine underTest;
+	int zeroBalance = 0;
+	int penny = 1;
+	int nickel = 5;
+	int dime = 10;
+	int quarter = 25;
+	int sumOfCoins = penny + nickel + dime + quarter;
 
 	@BeforeEach
 	void setUp() {
@@ -21,55 +27,44 @@ class VendingMachineTests {
 
 	@Test
 	void acceptPennyCoin() {
-		int coin = 1;
-		underTest.insertCoin(coin);
-		assertEquals(coin, underTest.getBalance());
+		underTest.insertCoin(penny);
+		assertEquals(penny, underTest.getBalance());
 	}
 
 	@Test
 	void acceptNickelCoin() {
-		int coin = 5;
-		underTest.insertCoin(coin);
-		assertEquals(coin, underTest.getBalance());
+		underTest.insertCoin(nickel);
+		assertEquals(nickel, underTest.getBalance());
 	}
 
 	@Test
 	void acceptDimeCoin() {
-		int coin = 10;
-		underTest.insertCoin(coin);
-		assertEquals(coin, underTest.getBalance());
+		underTest.insertCoin(dime);
+		assertEquals(dime, underTest.getBalance());
 	}
 
 	@Test
 	void acceptQuarterCoin() {
-		int coin = 25;
-		underTest.insertCoin(coin);
-		assertEquals(coin, underTest.getBalance());
+		underTest.insertCoin(quarter);
+		assertEquals(quarter, underTest.getBalance());
 	}
 
 	@Test
 	void doNotAcceptUnknownCoin() {
 		int coin = 7;
 		underTest.insertCoin(coin);
-		int zero = 0;
-		assertEquals(zero, underTest.getBalance());
+		assertEquals(zeroBalance, underTest.getBalance());
 	}
 
 	@Test
 	void doNotAcceptNegativeCoin() {
 		int coin = -10;
 		underTest.insertCoin(coin);
-		int zero = 0;
-		assertEquals(zero, underTest.getBalance());
+		assertEquals(zeroBalance, underTest.getBalance());
 	}
 
 	@Test
 	void moreCoinsAcceptedInSuccession() {
-		int penny = 1;
-		int nickel = 5;
-		int dime = 10;
-		int quarter = 25;
-		int sumOfCoins = penny + nickel + dime + quarter;
 		underTest.insertCoin(penny);
 		underTest.insertCoin(nickel);
 		underTest.insertCoin(dime);
@@ -116,11 +111,6 @@ class VendingMachineTests {
 
 	@Test
 	void refundGivesBackMoney() {
-		int penny = 1;
-		int nickel = 5;
-		int dime = 10;
-		int quarter = 25;
-		int sumOfCoins = penny + nickel + dime + quarter;
 		underTest.insertCoin(penny);
 		underTest.insertCoin(nickel);
 		underTest.insertCoin(dime);
@@ -133,7 +123,6 @@ class VendingMachineTests {
 		int coin = 10;
 		underTest.insertCoin(coin);
 		underTest.refund();
-		int zero = 0;
-		assertEquals(zero, underTest.getBalance());
+		assertEquals(zeroBalance, underTest.getBalance());
 	}
 }
