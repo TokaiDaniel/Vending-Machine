337db9621f056d169dbdde49405cc078c8d0ab62
Daniel Tokai <tokaidaniel@gmail.com>
2021-10-20 13:39:46 +0200

    Vending machine insertCoin method implemented

--- src/main/java/com/irems/vendingMachine/VendingMachine/VendingMachine.java
+++ src/main/java/com/irems/vendingMachine/VendingMachine/VendingMachine.java
@@ -1,9 +1,23 @@
 package com.irems.vendingMachine.VendingMachine;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class VendingMachine {
+
+    private int balance = 0;
+
+    public void insertCoin(Integer value) {
+        AcceptedCoin.stream()
+                .filter(coin -> coin.getValue().equals(value))
+                .findFirst().ifPresent(coin -> balance += coin.getValue());
+    }
 }
