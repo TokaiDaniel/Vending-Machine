b8359ec25966f24fb4a89355a71c9fb11a4c6601
Daniel Tokai <tokaidaniel@gmail.com>
2021-10-20 11:28:06 +0200

    main method renamed

--- src/main/java/com/irems/vendingMachine/Main.java
+++ src/main/java/com/irems/vendingMachine/Main.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VendingMachineApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VendingMachineApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
