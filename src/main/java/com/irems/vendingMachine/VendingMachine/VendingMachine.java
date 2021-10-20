03e074baacc08e0334136af41a3430538f1d7e44
Daniel Tokai <tokaidaniel@gmail.com>
2021-10-20 11:38:17 +0200

    Vending Machine class created

---
+++ src/main/java/com/irems/vendingMachine/VendingMachine/VendingMachine.java
@@ -0,0 +1,9 @@
+package com.irems.vendingMachine.VendingMachine;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class VendingMachine {
+}
