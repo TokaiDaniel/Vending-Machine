dd5fc4d4a5c3ebbe3d478f4feba60c78b3ae22d1
Daniel Tokai <tokaidaniel@gmail.com>
2021-10-21 17:04:29 +0200

    tests for AcceptedCoin added

--- src/test/java/com/irems/vendingMachine/AcceptedCoinTests.java
+++ src/test/java/com/irems/vendingMachine/AcceptedCoinTests.java
@@ -1,2 +1,49 @@
-package com.irems.vendingMachine;public class AcceptedCoinTests {
+package com.irems.vendingMachine;
+
+import com.irems.vendingMachine.VendingMachine.AcceptedCoin;
+import com.irems.vendingMachine.VendingMachine.NoSuchCoinException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class AcceptedCoinTests {
+    int penny = 1;
+    int nickel = 5;
+    int dime = 10;
+    int quarter = 25;
+
+    @Test
+    void convertValueToCoinAcceptsPennyCoin() throws NoSuchCoinException {
+        assertEquals(penny, AcceptedCoin.convertValueToCoin(penny).getValue());
+    }
+
+    @Test
+    void convertValueToCoinAcceptsPennyNickel() throws NoSuchCoinException {
+        assertEquals(nickel, AcceptedCoin.convertValueToCoin(nickel).getValue());
+    }
+
+    @Test
+    void convertValueToCoinAcceptsDimeCoin() throws NoSuchCoinException {
+        assertEquals(dime, AcceptedCoin.convertValueToCoin(dime).getValue());
+    }
+
+    @Test
+    void convertValueToCoinAcceptsQuarterCoin() throws NoSuchCoinException {
+        assertEquals(quarter, AcceptedCoin.convertValueToCoin(quarter).getValue());
+    }
+
+    @Test
+    void convertValueToCoinDoNotAcceptUnknownCoin() {
+        int coin = 7;
+        assertThrows(NoSuchCoinException.class, () -> AcceptedCoin.convertValueToCoin(coin));
+    }
+
+    @Test
+    void convertValueToCoinDoNotAcceptNegativeCoin() {
+        int coin = -10;
+        assertThrows(NoSuchCoinException.class, () -> AcceptedCoin.convertValueToCoin(coin));
+    }
 }
